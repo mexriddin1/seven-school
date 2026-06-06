0f3c93dc88769652ef8913b3f38d0aa57ca63240
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-06-07 00:45:33 +0500

    done analytics

--- client-site/src/app/layout.tsx
+++ client-site/src/app/layout.tsx
@@ -1,21 +1,44 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import Script from "next/script";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Seven School',
+  title: "Seven School",
   description: "Toshkentdagi xususiy maktab va bog'cha",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="uz">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EC7BH10HJW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EC7BH10HJW');
+          `}
+        </Script>
       </head>
       <body>{children}</body>
     </html>

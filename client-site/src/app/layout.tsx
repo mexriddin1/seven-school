c510fb366bdc2831c93797f6083f49c53d1825e7
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-06-08 23:26:23 +0500

    done

--- client-site/src/app/layout.tsx
+++ client-site/src/app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
   return (
     <html lang="uz">
       <head>
+        <meta
+          name="google-site-verification"
+          content="Mm679pRMxX7mNTuiwYtvcfDmtQ0bdTALaGZ_9p8NI20"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -37,6 +41,18 @@ export default function RootLayout({
             gtag('js', new Date());
 
             gtag('config', 'G-EC7BH10HJW');
+
+            // Telefon raqamlarini kuzatish
+            document.addEventListener("click", function(e) {
+              const link = e.target.closest('a[href^="tel:"]');
+              if (link && typeof gtag === 'function') {
+                gtag('event', 'phone_click', {
+                  'event_category': 'Contact',
+                  'event_label': link.href,
+                  'transport_type': 'beacon'
+                });
+              }
+            });
           `}
         </Script>
       </head>

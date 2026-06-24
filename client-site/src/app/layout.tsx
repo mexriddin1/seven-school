7bb328ceae2b180255c1e0552fecac5f97f9788f
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-06-24 21:01:17 +0500

    done

--- client-site/src/app/layout.tsx
+++ client-site/src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="uz">
       <head>
-        <meta name="facebook-domain-verification" content="wc0driq0rj8tz9wv4hrkfdczg9lzre" />
+        <meta
+          name="facebook-domain-verification"
+          content="wc0driq0rj8tz9wv4hrkfdczg9lzre"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -32,7 +35,27 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NHVFHZCC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
+
+        {/* Google Tag Manager - Head Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NHVFHZCC');`}
+        </Script>
 
         {/* Google Analytics va tracking skriptlari body oxiriga ko'chirildi */}
         <Script
